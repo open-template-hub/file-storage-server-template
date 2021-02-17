250c65043f43950e22b11aeb66610a1d7b38d962
furknyavuz <furknyavuz@gmail.com>
2021-02-17 18:43:00 +0300

    Rename collection

--- app/data/service-provider.data.ts
+++ app/data/service-provider.data.ts
@@ -5,7 +5,7 @@
 import mongoose from 'mongoose';
 
 export class ServiceProviderDataModel {
-  private readonly collectionName: string = 'service-provide';
+  private readonly collectionName: string = 'service_provider';
 
   private productSchema: mongoose.Schema;
 
