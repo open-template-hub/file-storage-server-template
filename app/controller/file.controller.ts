22f4c0a3b21def47dd6bfacc0812abfc93ea5993
Fatih Turker <fatihturker35@gmail.com>
2022-12-21 13:44:07 +0300

    Fix team file reporter

--- app/controller/file.controller.ts
+++ app/controller/file.controller.ts
@@ -60,6 +60,10 @@ export class FileController {
       context.serviceKey
     );
 
+    if (!(file.reporter && file.reporter.length > 0)) {
+      file.reporter = teamId;
+    }
+
     file = await serviceClient.service.upload(serviceClient.client, file);
 
     if (file.uploaded) {
